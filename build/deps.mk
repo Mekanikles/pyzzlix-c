faa618a86c9353ab2b17acbda6fe7575425f711c
Joel Lennartsson <joele375@student.liu.se>
2010-11-26 03:23:00 +0100

    obj files are now placed in each build directory

--- build/deps.mk
+++ build/deps.mk
@@ -1,12 +1,11 @@
+SRCPATH=../../src
+
 
 SOURCE= main.cpp renderer.cpp scene.cpp scenehandler.cpp sprite.cpp
 SOURCE+= scene_maingame.cpp
 
 DEPS= image.h linkedlist.h point.h vector.h scene.h scenehandler.h sprite.h texture.h time.h vector.h
 DEPS+= scene_maingame.h
-
-SRCPATH=../../src
-
 
 REALSOURCE = $(addprefix $(SRCPATH)/, $(SOURCE))
 REALDEPS = $(addprefix $(SRCPATH)/, $(SOURCE))
